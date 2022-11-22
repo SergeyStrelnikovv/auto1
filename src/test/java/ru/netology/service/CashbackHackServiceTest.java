a5199a18750df205adba9b64eb36a6cbdda95b6a
sergey <sso96@mail.ru>
2022-11-22 15:10:45 +0300

    Junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,8 @@
 package ru.netology.service;
 
-
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -30,29 +29,5 @@ public class CashbackHackServiceTest {
         int expected = 0;
         int actual = service.remain(amount);
         assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    public void shouldRecommendToAddWhenUnderBoundaryJUnit5() {
-        int amount = 900;
-        int expected = 100;
-        int actual = service.remain(amount);
-        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    public void shouldRecommendToAddWhenOverBoundaryJUnit5() {
-        int amount = 2500;
-        int expected = 500;
-        int actual = service.remain(amount);
-        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    public void shouldNotRecommendToAddJUnit5() {
-        int amount = 1000;
-        int expected = 0;
-        int actual = service.remain(amount);
-        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 }
